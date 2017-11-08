1a1f55b6cfd8040fb31dec66559f91d7bc607f8a
vl-nix <33422103+vl-nix@users.noreply.github.com>
2017-11-08 10:50:14 +0200

    Update gtv.h

--- src/gtv.h
+++ src/gtv.h
@@ -13,20 +13,22 @@
 #include <glib/gstdio.h>
 
 
-gchar *rec_dir, *audio_encoder, *video_encoder, *muxer, *file_ext;
-
-GtkTreeView *tv_treeview;
 enum { COL_NUM, COL_FILES_CH, COL_URI_DATA, NUM_COLS };
 
 void tv_set_tuning_timeout ( GstElement *element );
 
+void tv_message_dialog ( gchar *f_error, gchar *file_or_info, GtkMessageType mesg_type );
+
 GtkScrolledWindow * tv_scroll_win ( GtkTreeView *tree_view, gchar *title, gchar *data );
+void tv_set_sgn_snr ( GstElement *element, GtkLabel *label, gdouble sgb, gdouble srb, gboolean hlook );
+
 void tv_add_channels ( const gchar *name_ch, gchar *data );
 void tv_str_split_ch_data ( gchar *data );
-void tv_set_sgn_snr ( GstElement *element, GtkLabel *label, gdouble sgb, gdouble srb, gboolean hlook );
 
 gchar * tv_openf ();
 gchar * tv_rec_dir ();
+
+gchar *rec_dir, *audio_encoder, *video_encoder, *muxer, *file_ext;
 
 
 #endif // GTV_H
